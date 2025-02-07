c4dd7fb5a438af3e5624aec5e2173890fe8e3d8e
Fredrick Oluoch <menty44@gmail.com>
2025-02-08 00:18:52 +0300

    Generated users rest resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/auth-workshop'),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
